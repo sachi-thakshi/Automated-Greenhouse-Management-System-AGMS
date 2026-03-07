b0214f1ef9a3179c6575a08794f2373982a58955
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-07 22:39:49 +0530

    Infrastructure: Completed Automation Service setup.

---
+++ agms-automation-service/src/test/java/lk/ijse/agmsautomationservice/AgmsAutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.agmsautomationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgmsAutomationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
