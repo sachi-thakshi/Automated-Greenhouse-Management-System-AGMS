f79fdb943e1232d2f25099bdc318f1cec1d1c861
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-25 21:30:25 +0530

    feat(crop): add ModelMapper bean for DTO-Entity conversion

---
+++ agms-crop-service/src/main/java/lk/ijse/agmscropservice/config/AppConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.agmscropservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
