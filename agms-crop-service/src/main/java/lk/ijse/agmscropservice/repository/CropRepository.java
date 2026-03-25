789c96c1cdc20ba2d3f2e365f698a5cabe81371f
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-25 22:13:31 +0530

    feat(crop): implement MongoRepository with custom zoneId query

--- agms-crop-service/src/main/java/lk/ijse/agmscropservice/repository/CropRepository.java
+++ agms-crop-service/src/main/java/lk/ijse/agmscropservice/repository/CropRepository.java
@@ -2,7 +2,8 @@ package lk.ijse.agmscropservice.repository;
 
 import lk.ijse.agmscropservice.entity.Crop;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface CropRepository extends MongoRepository<Crop, String> {
-
+    List<Crop> findByZoneId(Long zoneId);
 }
