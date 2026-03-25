ec88dcc0dc971bcb55033d61eb2ab2ceadc502e9
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-25 22:00:35 +0530

    feat(crop): create Crop Document for MongoDB with zoneId reference

--- agms-crop-service/src/main/java/lk/ijse/agmscropservice/entity/Crop.java
+++ agms-crop-service/src/main/java/lk/ijse/agmscropservice/entity/Crop.java
@@ -1,56 +1,19 @@
 package lk.ijse.agmscropservice.entity;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "crops")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class Crop {
-
     @Id
     private String id;
     private String cropName;
-    private String season;
-    private String fieldLocation;
-
-    public Crop() {
-    }
-
-    public Crop(String id, String cropName, String season, String fieldLocation) {
-        this.id = id;
-        this.cropName = cropName;
-        this.season = season;
-        this.fieldLocation = fieldLocation;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCropName() {
-        return cropName;
-    }
-
-    public void setCropName(String cropName) {
-        this.cropName = cropName;
-    }
-
-    public String getSeason() {
-        return season;
-    }
-
-    public void setSeason(String season) {
-        this.season = season;
-    }
-
-    public String getFieldLocation() {
-        return fieldLocation;
-    }
-
-    public void setFieldLocation(String fieldLocation) {
-        this.fieldLocation = fieldLocation;
-    }
+    private String scientificName;
+    private String category;
+    private String growthStatus;
+    private String cropSeason;
+    private Long zoneId;
 }
