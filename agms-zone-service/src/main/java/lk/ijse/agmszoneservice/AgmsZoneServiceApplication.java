f3a788454c1bea79094105e0fef1b8c78ceda0b2
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-25 20:26:17 +0530

    perf: add 3-second timeouts to RestTemplate to prevent blocking

--- agms-zone-service/src/main/java/lk/ijse/agmszoneservice/AgmsZoneServiceApplication.java
+++ agms-zone-service/src/main/java/lk/ijse/agmszoneservice/AgmsZoneServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,5 +19,15 @@ public class AgmsZoneServiceApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        factory.setConnectTimeout(3000);
+        factory.setConnectionRequestTimeout(3000);
+
+        return new RestTemplate(factory);
     }
 }
