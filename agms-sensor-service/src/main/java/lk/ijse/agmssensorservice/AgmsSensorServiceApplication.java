6b346bd7ce2710eba8d31e6a2cf9f93c32156d04
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-26 13:51:14 +0530

    feat(sensor): enable scheduling and discovery in main class

--- agms-sensor-service/src/main/java/lk/ijse/agmssensorservice/AgmsSensorServiceApplication.java
+++ agms-sensor-service/src/main/java/lk/ijse/agmssensorservice/AgmsSensorServiceApplication.java
@@ -1,25 +1,15 @@
 package lk.ijse.agmssensorservice;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @EnableScheduling
 public class AgmsSensorServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AgmsSensorServiceApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 }
