653dc1c93640442dda9028aa2d200e8651d71b48
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2026-03-26 14:21:10 +0530

    feat(automation): initialize main application with service discovery

--- agms-automation-service/src/main/java/lk/ijse/agmsautomationservice/AgmsAutomationServiceApplication.java
+++ agms-automation-service/src/main/java/lk/ijse/agmsautomationservice/AgmsAutomationServiceApplication.java
@@ -3,12 +3,11 @@ package lk.ijse.agmsautomationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class AgmsAutomationServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AgmsAutomationServiceApplication.class, args);
     }
